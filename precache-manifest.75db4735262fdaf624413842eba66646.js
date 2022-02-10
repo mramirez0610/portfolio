6866c22bfd0c0a15598aef9ac68a63b3e99a78f0
marco ramirez <77829612+mramirez0610@users.noreply.github.com>
2022-02-10 15:28:59 -0500

    Updates

---
+++ precache-manifest.75db4735262fdaf624413842eba66646.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cb3b4213fc306a93a3ac432b7a73c4f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e7236353da58a9138ee6",
+    "url": "/portfolio/static/css/main.e06b02bb.chunk.css"
+  },
+  {
+    "revision": "7da278c41924b11db819",
+    "url": "/portfolio/static/js/2.6371ef25.chunk.js"
+  },
+  {
+    "revision": "03acf47b1c933fdd5274",
+    "url": "/portfolio/static/js/3.0ad1fd9d.chunk.js"
+  },
+  {
+    "revision": "e7236353da58a9138ee6",
+    "url": "/portfolio/static/js/main.d452680d.chunk.js"
+  },
+  {
+    "revision": "dddedfc018ccec1c42fd",
+    "url": "/portfolio/static/js/runtime~main.92ef72b9.js"
+  }
+]);
