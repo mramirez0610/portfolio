b2565ab0ccc4e576ab6fe8a12888daeb078bd7d1
marco ramirez <77829612+mramirez0610@users.noreply.github.com>
2022-02-12 17:53:38 -0500

    Updates

---
+++ precache-manifest.f446a005d7dd9fc5c0cba9416df4336f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3c4f86def8fe44bbc422c17005f88be",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3397683ff44d0bf38f7e",
+    "url": "/portfolio/static/css/main.0af3ee23.chunk.css"
+  },
+  {
+    "revision": "5c0fae890e215fb3ccb5",
+    "url": "/portfolio/static/js/2.1fd32186.chunk.js"
+  },
+  {
+    "revision": "2ded26446bc94b81347a",
+    "url": "/portfolio/static/js/3.398cb210.chunk.js"
+  },
+  {
+    "revision": "3397683ff44d0bf38f7e",
+    "url": "/portfolio/static/js/main.097beffe.chunk.js"
+  },
+  {
+    "revision": "c6c215238f56b36f30c3",
+    "url": "/portfolio/static/js/runtime~main.354373da.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.075a2c946e008c7c3bf2bc3056e51f37.js"
+  "/portfolio/precache-manifest.f446a005d7dd9fc5c0cba9416df4336f.js"
 );
 
 self.addEventListener('message', (event) => {
