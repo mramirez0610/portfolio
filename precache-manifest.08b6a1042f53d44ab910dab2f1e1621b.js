893fcbbfdc46690e5068caf32daeb963cb779527
marco ramirez <77829612+mramirez0610@users.noreply.github.com>
2022-02-27 22:20:14 -0500

    Updates

--- precache-manifest.08b6a1042f53d44ab910dab2f1e1621b.js
+++ precache-manifest.08b6a1042f53d44ab910dab2f1e1621b.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "575f298108367f8d49abfede3b79fcb6",
+    "revision": "cb4ae1dde30025b4a27a72d41001644c",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "7df5bdcad639c0df4c99",
+    "revision": "f03b8d3ea865746e1d15",
     "url": "/portfolio/static/css/main.66359078.chunk.css"
   },
   {
-    "revision": "9e384f99d983c8447420",
-    "url": "/portfolio/static/js/2.95afe6e8.chunk.js"
+    "revision": "1f15db9c24d1ce369a63",
+    "url": "/portfolio/static/js/2.c428ca3b.chunk.js"
   },
   {
     "revision": "2ded26446bc94b81347a",
     "url": "/portfolio/static/js/3.398cb210.chunk.js"
   },
   {
-    "revision": "7df5bdcad639c0df4c99",
-    "url": "/portfolio/static/js/main.22eee9e7.chunk.js"
+    "revision": "f03b8d3ea865746e1d15",
+    "url": "/portfolio/static/js/main.c887cc21.chunk.js"
   },
   {
     "revision": "c6c215238f56b36f30c3",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.3b3eb54e58f7789762f33276c5394b77.js"
+  "/portfolio/precache-manifest.08b6a1042f53d44ab910dab2f1e1621b.js"
 );
 
 self.addEventListener('message', (event) => {
